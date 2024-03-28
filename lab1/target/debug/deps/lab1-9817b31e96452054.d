9960aef0c029674f9b93eab29063d31b2fe8416f
Papekoja <william.reit@gmail.com>
2024-03-28 15:09:06 +0100

    changes

--- lab1/target/debug/deps/lab1-9817b31e96452054.d
+++
@@ -1,6 +0,0 @@
-/Users/williamreit/programming/school/EDAN55-labs/lab1/target/debug/deps/liblab1-9817b31e96452054.rmeta: src/main.rs src/r.rs
-
-/Users/williamreit/programming/school/EDAN55-labs/lab1/target/debug/deps/lab1-9817b31e96452054.d: src/main.rs src/r.rs
-
-src/main.rs:
-src/r.rs:
